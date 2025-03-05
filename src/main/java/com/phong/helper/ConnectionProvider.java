c45897382de62f0721df8a8a9087b6893c925564
Windyphan <thandongdatviettatca@gmail.com>
2025-03-05 08:27:14 +0700

    Migrate mysql into postgresql

--- src/main/java/com/phong/helper/ConnectionProvider.java
+++ src/main/java/com/phong/helper/ConnectionProvider.java
@@ -4,8 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import javax.servlet.http.HttpServlet;
 
-public class ConnectionProvider extends HttpServlet{
-	
+public class ConnectionProvider extends HttpServlet {
+
 	private static final long serialVersionUID = 1L;
 	private static Connection connection;
 
@@ -13,13 +13,15 @@ public class ConnectionProvider extends HttpServlet{
 
 		try {
 			if (connection == null) {
-				Class.forName("com.mysql.cj.jdbc.Driver");
-				connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/phong", "root", "root");
+				Class.forName("org.postgresql.Driver"); // PostgreSQL Driver
+				connection = DriverManager.getConnection(
+						"jdbc:postgresql://localhost:5432/phong", // PostgreSQL JDBC URL
+						"postgres", // PostgreSQL username
+						"postgres"); // PostgreSQL password
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return connection;
 	}
-
 }
